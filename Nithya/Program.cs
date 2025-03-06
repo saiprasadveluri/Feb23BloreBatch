3560db1039e916997de515377a80721f67037c99
Nithyarr <138715092+Nithyarr@users.noreply.github.com>
2025-03-06 18:25:31 +0530

    Front_Desk

---
+++ Nithya/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrontDesk manager = new FrontDesk();
+            while (true)
+            {
+                Console.WriteLine("1. Add Room");
+                Console.WriteLine("2. Display Rooms");
+                Console.WriteLine("3. Display Room Details");
+                Console.WriteLine("4. Remove Room");
+                Console.WriteLine("5.add participants");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Enter your choice");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter Room Name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter Room Capacity");
+                        int capacity = int.Parse(Console.ReadLine());
+                        manager.AddRoom(name, capacity);
+                        break;
+                    case "2":
+                        manager.DisplayRooms();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter Room Name");
+                        string roomName = Console.ReadLine();
+                        manager.DisplayRoomDetails(roomName);
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter Room Name");
+                        string roomName1 = Console.ReadLine();
+                        manager.RemoveRoom(roomName1);
+                        break;
+                    case "5":
+                        Console.WriteLine("Enter Room Name");
+                        string roomName2 = Console.ReadLine();
+                        Console.WriteLine("Enter Participant Name");
+                        string participantName = Console.ReadLine();
+                        manager.AddParticipant(roomName2, participantName);
+                        break;
+                    case "6":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Nithya/room.cs
@@ -0,0 +1,62 @@
+﻿using FrontDesk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk
+{
+    public abstract class room
+    {
+        public string RoomId { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public bool Availability { get; set; }
+        public string[] Participants { get; set; }
+        public int ParticipantCount { get; set; }
+        public abstract room(string roomId, string name, int capacity)
+        {
+            RoomId = roomId;
+            Name = name;
+            Capacity = capacity;
+            Availability = true;
+            Participants = new string[capacity];
+            ParticipantCount = 0;
+        }
+        public bool AddParticipant(string participantName)
+        {
+            if (ParticipantCount < Capacity)
+            {
+                Participants[ParticipantCount] = participantName;
+                ParticipantCount++;
+                return true;
+            }
+            return false;
+        }
+        public void ListParticipants()
+        {
+            Console.WriteLine($"Participants:{Name}");
+            for (int i = 0; i < ParticipantCount; i++)
+            {
+                Console.WriteLine(Participants[i]);
+            }
+        }
+    
+
+    public abstract void DisplayDetails();
+    }
+}
+
+
+public class meetingRoom : room
+{
+    public meetingRoom(string roomId, string name, int capacity) : base(roomId, name, capacity)
+    {
+    }
+    public override void DisplayDetails()
+    {
+        Console.WriteLine($"Room Id:{RoomId}-Capacity: {Capacity},Availablity:{Availabitlity}");
+        
+    }
+}
