f9b1bd6315a74742af6e1a38dcc1315c698806b1
Ayush-sharma1234 <101088519+Ayush-sharma1234@users.noreply.github.com>
2025-03-07 17:28:25 +0530

    Add files via upload
    FoodDelivery App

---
+++ RestaurantDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery
+{
+    public class RestaurantDTO
+    {
+        public long RID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public long OwnerId { get; set; }
+    }
+}
