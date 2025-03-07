75f80f8e2b0b66b40990d3a0a1390a66d213c6c6
saiprasadveluri <59499218+saiprasadveluri@users.noreply.github.com>
2025-03-07 13:06:19 +0530

    Iteration-1

---
+++ Code Examples/FoodDelApp/FoodDelApp/DataAccessLayer.cs
@@ -0,0 +1,93 @@
+﻿using FoodDelApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace FoodDelApp
+{
+    public class DataAccessLayer
+    {
+        const string ConString = "Data Source=.;Initial Catalog=MFoodDelDB;Integrated Security=SSPI";
+        SqlConnection con = null;
+        public DataAccessLayer()
+        {
+            con = new SqlConnection(ConString);
+            con.Open();
+        }
+
+        public void CloseApp()
+        {
+            if (con != null)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        public UserDTO Login(string Email, string Password)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"SELECT * FROM UserInfo where Email='{Email}' AND Password='{Password}'";
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                UserDTO user = new UserDTO();
+                user.UserId = reader.GetInt64(0);
+                user.Name = reader.GetString(1);
+                user.RoleName = reader.GetString(4);
+                user.Location = reader.GetString(5);
+                reader.Close();
+                return user;
+            }
+            reader.Close();
+            return null;
+        }
+
+        public bool AddNewRestaurant(RestaurantDTO restaurant)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"INSERT INTO Restaurents (RName,Location,OwnerId) VALUES('{restaurant.Name}','{restaurant.Location}',{restaurant.OwnerId}";
+            int RowsEffected=cmd.ExecuteNonQuery();
+            if (RowsEffected > 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool AddNewUser(UserDTO user)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"INSERT INTO UserInfo (DisplayName,Email,Password,RoleName,Location) VALUES('{user.Name}','{user.Email}','{user.Password}','{user.RoleName}','{user.Location}')";
+            int RowsEffected = cmd.ExecuteNonQuery();
+            if (RowsEffected > 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public string GetUserRole(long UserId)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"Select RoleName from UserInfo where UserId={UserId}";
+            object res=cmd.ExecuteScalar();
+            if (res != null)
+            {
+                return res.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
