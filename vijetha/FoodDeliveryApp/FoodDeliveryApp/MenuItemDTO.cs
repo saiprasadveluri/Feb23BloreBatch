fe76aca25f239bfa88ec5845697781bd25ca34be
vijethashivaraju <148524889+vijethashivaraju@users.noreply.github.com>
2025-03-11 16:42:02 +0530

    Add files via upload

---
+++ vijetha/FoodDeliveryApp/FoodDeliveryApp/MenuItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDeliveryApp
+{
+    public class MenuItemDTO
+    {
+        public long MID { get; set; }
+        public string MenuName { get; set; }
+        public long RID { get; set; }
+        public double UnitPrice { get; set; }
+        public string FoodType { get; set; }
+    }
+}
---
+++ vijetha/FoodDeliveryApp/FoodDeliveryApp/OrderLineDataDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDeliveryApp
+{
+    public class OrderLineDataDTO
+    {
+        public long MenuId { get; set; }
+        public int Qty { get; set; }
+    }
+}
