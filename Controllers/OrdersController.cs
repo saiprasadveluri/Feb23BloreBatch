29eb6b67ad7ec59222138c4946080c6a1d0dd27c
madhusnehashrees <kthjjkmss@gmail.com>
2025-03-13 11:28:09 -0700

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,77 @@
+﻿using FoodDelApp;
+using FoodDelApp.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using MMVCDemoApp1.Infra;
+using MMVCDemoApp1.Models;
+using System.Security.Cryptography;
+
+namespace MMVCDemoApp1.Controllers
+{
+    public class OrdersController : Controller
+    {
+        BusinessLayer bl;
+        public OrdersController(BusinessLayer businessLayer)
+        {
+            bl = businessLayer;
+        }
+        public IActionResult Index(int Id)
+        {
+            ObjectJsonHelper.RemoveSessionItem(HttpContext, "OrderData");
+            List<MenuItemDTO> menuItems = bl.GetRestaurentMenu(Id);
+            OrderViewModel omodel = new OrderViewModel();
+            omodel.MenuItems = menuItems;
+            omodel.RID = Id;
+            return View(omodel);
+        }
+
+        [HttpPost]
+        public IActionResult OrderSummary(int RID, string[] mids)
+        {
+            List<MenuItemDTO> menuItems = bl.GetRestaurentMenu(RID);
+            OrderSummaryViewModel model = new();
+            List<OrderLineData> OrderItems = new List<OrderLineData>();
+            model.RID = RID;
+            double OrderTotal = 0.0;
+            foreach (string mid in mids)
+            {
+                string QtyId = $"qty_{mid}";
+                string qty = HttpContext.Request.Form[QtyId][0];
+                int nQty = int.Parse(qty);
+                OrderLineData old = new OrderLineData();
+                if (nQty > 0)
+                {
+
+                    long menuId = long.Parse(mid);
+                    string MenuName = menuItems.Where(m => m.MID == menuId).Select(m => m.MenuName).FirstOrDefault();
+                    double Price = menuItems.Where(m => m.MID == menuId).Select(m => m.UnitPrice).FirstOrDefault();
+                    old.MenuId = menuId;
+                    old.MenuName = MenuName;
+                    old.Qty = nQty;
+                    old.UnitTotal = nQty * Price;
+                    OrderTotal += old.UnitTotal;
+                    OrderItems.Add(old);
+                }
+            }
+            if (OrderItems.Count > 0)
+            {
+                model.OrderdLineItems = OrderItems;
+                model.OrderTotal = OrderTotal;
+                ObjectJsonHelper.AddToSession(HttpContext, "OrderData", model);
+                return View(model);
+            }
+            return RedirectToAction("Index", new { Id = RID });
+        }
+
+        public IActionResult ConfirmOrder()
+        {
+            OrderSummaryViewModel model = ObjectJsonHelper.GetFromSession<OrderSummaryViewModel>(HttpContext, "OrderData");
+            UserDTO loggedUser = ObjectJsonHelper.GetFromSession<UserDTO>(HttpContext, "LoggedInUser");
+            if (model != null && loggedUser != null)
+            {
+                bool Status = bl.PlaceOrder(loggedUser, model.RID, model.OrderdLineItems);
+                return RedirectToAction("Index", "UserDashboard");
+            }
+            return RedirectToAction("Index", "UserDashboard");
+        }
+    }
+}
---
+++ Controllers/OwnerDashboardController.cs
@@ -0,0 +1,30 @@
+﻿using FoodDelApp;
+using FoodDelApp.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using MMVCDemoApp1.Infra;
+
+namespace MMVCDemoApp1.Controllers
+{
+    public class OwnerDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            string strUserInfo = HttpContext.Session.GetString("LoggedInUser");
+            if (strUserInfo == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            UserDTO luser = ObjectJsonHelper.GetFromJson<UserDTO>(strUserInfo);
+            if (luser.RoleName != "OWNER")
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            BusinessLayer bl = new BusinessLayer();
+            List<RestaurantDTO> RList = bl.GetAllRestaurants();
+
+            bl.GetAllRestaurants();
+            return View();
+        }
+    }
+}
